846c5cadd005b7324bee8dd11dd09f5e840df87e
matlencar <malencar292@gmail.com>
2021-11-10 10:03:04 -0300

    inserindo localhost e deletando spec tema

--- frontBlogMatheus/blogPessoal/src/app/service/auth.service.ts
+++ frontBlogMatheus/blogPessoal/src/app/service/auth.service.ts
@@ -15,12 +15,12 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin>{ 
-    return this.http.post<UserLogin>('https://lojadegibis.herokuapp.com/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
 
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('https://lojadegibis.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
 
   }
 
